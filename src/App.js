9ad6f2ec79011ed8b2d5c2573d4507fc3be63535
Teddy <t.boirin@icloud.com>
2020-05-02 00:42:55 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import './reset.css';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import Header from './components/Header/Header'
 import Footer from "./components/Footer/Footer";
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Header></Header>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './Header.scss';
+
+
+
+export default function Header(props) {
+
+ 
+  let openMenu = () => {
+    let menu = document.getElementById('menu');
+      menu.classList.toggle('open');
+  }
+
+  return (
+
+
+   <div>
+      <header>
+      <div className="logo">
+        Gamovic
+      </div>
+      <div className="menu" onClick={openMenu}>
+        <div className="bloc-menu">
+            <div className="line-menu"></div>
+            <div className="line-menu second"></div>
+            <div className="line-menu three"></div>
+        </div>
+      </div>
+
+    </header>
+
+    <div className="menu-open" id="menu">
+      <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Informations</a></li>
+        <li><a href="">Inscription</a></li>
+        <li><a href="">Connexion</a></li>
+        <li><a href="">Nous contacter</a></li>
+      </ul>
+    </div>
+   </div>
+
+
+  )
+}
+
