bb161f5f4af2439153849cf4bcf00ff67ed93fdb
Haitamrane <hassane.aamrane@gmail.com>
2020-05-05 04:44:18 +0200

    Homefinish(

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,30 @@ class Footer extends React.Component {
     render() {
         return (
             <footer>
+                <h1>Gamovic</h1>
+            <div className="right-left">
+                <div className="left">
+                    <ul>
+                        <li>Tournois</li>
+                        <li>Nous contacter</li>
+                        <li>Informations légales</li>
+                    </ul>
+                </div>
 
+                <div className="right">
+                    <ul>
+                        <li>L'équipe</li>
+                        <li>Réglements</li>
+                        <li>Nous rejoindre</li>
+                    </ul>
+                </div>
+            </div> 
+
+                <div className="bottom">
+                        <p>instagram</p>
+                        <p>twitch</p>
+                        <p>twitter</p>
+                </div>
             </footer>
         );
     }
